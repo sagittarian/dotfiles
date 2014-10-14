354d1d8cdbaf6a8cd8289e77887cd48cdeeb7abe
Adam Mesha <adam@mesha.org>
2014-10-14 10:34:07 +0300

    change .emacs mentions to init in init.el

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -464,5 +464,7 @@
 (put 'downcase-region 'disabled nil)
 (put 'upcase-region 'disabled nil)
 
-(provide '.emacs)
-;;; .emacs ends here
+(load "keybindings.el")
+
+(provide 'init)
+;;; init ends here
