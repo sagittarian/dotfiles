eca98be2c0cfb306d3af0a73cab339d1f2e1c677
Adam Mesha <adam@mesha.org>
2014-10-14 10:38:16 +0300

    a bunch of possibly useful keybindings taken from some prelude

--- home/.emacs.d/keybindings.el
+++ home/.emacs.d/keybindings.el
@@ -58,5 +58,93 @@
 ;; js2-refactor
 (js2r-add-keybindings-with-prefix "C-c C-m")
 
+;;;; miscelaneous stuff collected from the internets to go through and
+;;;; see if there's anything interesting there some time
+
+;;;;;; source: https://github.com/steinn/emacs-prelude/blob/master/core/prelude-global-keybindings.el
+
+;; ;; Align your code in a pretty way.
+;; (global-set-key (kbd "C-x \\") 'align-regexp)
+
+;; ;; Font size
+;; (global-set-key (kbd "C-+") 'text-scale-increase)
+;; (global-set-key (kbd "C--") 'text-scale-decrease)
+
+;; ;; Window switching. (C-x o goes to the next window)
+;; (global-set-key (kbd "C-x O") (lambda ()
+;;                                 (interactive)
+;;                                 (other-window -1))) ;; back one
+
+;; ;; Indentation help
+;; (global-set-key (kbd "C-x ^") 'join-line)
+
+;; ;; Start proced in a similar manner to dired
+;; (global-set-key (kbd "C-x p") 'proced)
+
+;; ;; Start eshell or switch to it if it's active.
+;; (global-set-key (kbd "C-x m") 'eshell)
+
+;; ;; Start a new eshell even if one is active.
+;; (global-set-key (kbd "C-x M") (lambda () (interactive) (eshell t)))
+
+;; ;; Start a regular shell if you prefer that.
+;; (global-set-key (kbd "C-x M-m") 'shell)
+
+;; ;; If you want to be able to M-x without meta
+;; (global-set-key (kbd "C-x C-m") 'execute-extended-command)
+
+;; ;; A complementary binding to the apropos-command (C-h a)
+;; (define-key 'help-command "A" 'apropos)
+
+;; (global-set-key (kbd "C-h C-f") 'find-function)
+;; (global-set-key (kbd "C-h C-k") 'find-function-on-key)
+;; (global-set-key (kbd "C-h C-v") 'find-variable)
+;; (global-set-key (kbd "C-h C-l") 'find-library)
+
+;; ;; a complement to the zap-to-char command, that doesn't eat up the target character
+;; (autoload 'zap-up-to-char "misc" "Kill up to, but not including ARGth occurrence of CHAR.")
+;; (global-set-key (kbd "M-Z") 'zap-up-to-char)
+
+;; ;; Activate occur easily inside isearch
+;; (define-key isearch-mode-map (kbd "C-o")
+;;   (lambda () (interactive)
+;;     (let ((case-fold-search isearch-case-fold-search))
+;;       (occur (if isearch-regexp
+;;                  isearch-string
+;;                (regexp-quote isearch-string))))))
+
+;; ;; use hippie-expand instead of dabbrev
+;; (global-set-key (kbd "M-/") 'hippie-expand)
+
+;; ;; replace buffer-menu with ibuffer
+;; (global-set-key (kbd "C-x C-b") 'ibuffer)
+
+;; ;; toggle fringe visibility
+;; (global-set-key (kbd "<f11>") 'fringe-mode)
+
+;; ;; toggle menu-bar visibility
+;; (global-set-key (kbd "<f12>") 'menu-bar-mode)
+
+;; (global-set-key (kbd "C-x g") 'magit-status)
+;; (global-set-key (kbd "C-c C-b") 'magit-blame-mode)
+
+;; (global-set-key (kbd "C-=") 'er/expand-region)
+
+;; (global-set-key (kbd "<f10>") 'toggle-debug-on-quit)
+
+;; ;; make C-x C-x usable with transient-mark-mode
+;; (define-key global-map
+;;   [remap exchange-point-and-mark]
+;;   'prelude-exchange-point-and-mark)
+
+;; (global-set-key (kbd "C-c SPC") 'ace-jump-mode)
+;; (global-set-key (kbd "C-x SPC") 'ace-jump-mode-pop-mark)
+
+;; (provide 'prelude-global-keybindings)
+
+;; ;;; prelude-global-keybindings.el ends here
+
+
+
 (provide 'keybindings)
 ;;; keybindings ends here
