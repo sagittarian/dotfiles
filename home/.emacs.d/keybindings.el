dcbc0bae62c276f73def865b328d369bcdb7a688
Adam Mesha <adam@mesha.org>
2014-10-15 13:12:55 +0300

    trying flex-isearch

--- home/.emacs.d/keybindings.el
+++ home/.emacs.d/keybindings.el
@@ -47,12 +47,15 @@
 (global-set-key (kbd "C-`") 'bury-buffer)
 ;; (global-set-key (kbd "<C-tab>") 'bury-buffer)
 (global-set-key (kbd "M-/") 'hippie-expand)
-(global-set-key (kbd "C-s") 'isearch-forward-regexp)
-(global-set-key (kbd "C-r") 'isearch-backward-regexp)
+
+;; trying flex-isearch
+;; (global-set-key (kbd "C-s") 'isearch-forward-regexp)
+;; (global-set-key (kbd "C-r") 'isearch-backward-regexp)
+;; (global-set-key (kbd "C-M-s") 'isearch-forward)
+;; (global-set-key (kbd "C-M-r") 'isearch-backward)
+
 (global-set-key (kbd "C-c %") 'replace-string)
 (global-set-key (kbd "C-c M-%") 'replace-regexp)
-(global-set-key (kbd "C-M-s") 'isearch-forward)
-(global-set-key (kbd "C-M-r") 'isearch-backward)
 (global-set-key (kbd "C-x j") 'auto-fill-mode)
 
 ;; js2-refactor
