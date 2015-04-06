7bc3eda7d27f619d12320b9650bfba4d1621aae1
Adam Mesha <adam@mesha.org>
2015-04-06 16:00:36 +0300

    allow adding a note to bookmarks

--- home/.local/share/uzbl/scripts/bookmark.py
+++ home/.local/share/uzbl/scripts/bookmark.py
@@ -5,7 +5,7 @@ import os
 import re
 import sys
 
-DEFAULT_OUT = os.path.expanduser('~/src/org/bookmarks')
+DEFAULT_OUT_DIR = os.path.expanduser('~/src/org')
 
 class AddTagTransform:
     def __init__(self, tag, test):
@@ -26,10 +26,23 @@ class Bookmark:
         cls.transformations.append(func)
         return func
 
-    def __init__(self, url, tags, title):
+    @staticmethod
+    def parsetags(note):
+        tags = []
+        def replace(m):
+            tags.append(m.group(2))
+            if not m.group(1) or not m.group(3):
+                return ''
+            return m.group(1) + m.group(3)
+        return tags, re.sub(r'(\s+|^)#(\S+)(\s+|$)', replace, note).strip()
+
+    def __init__(self, url='', tags=(), title='', note=''):
+        extratags, note = self.parsetags(note)
         self.url = url
-        self.tags = tags
+        self.tags = list(tags)
+        self.tags.extend(extratags)
         self.title = title
+        self.note = note
 
     def apply_transforms(self):
         for xfunc in self.transformations:
@@ -37,8 +50,12 @@ class Bookmark:
 
     def format_line(self):
         tags = ' '.join(self.hashtag + t for t in self.tags)
-        line = "{url} {tags} {title}".format(
-            url=self.url, title=self.title, tags=tags)
+        if not self.title or not self.note:
+            rest = self.title or self.note
+        else:
+            rest = '{title}: {note}'.format(title=self.title, note=self.note)
+        line = "{url} {tags} {rest}".format(
+            url=self.url, rest=rest, tags=tags)
         return line
 
 lesswrong = AddTagTransform('lw',
@@ -51,12 +68,15 @@ def get_args():
     is_uzbl = 'uzbl' in sys.argv[0]
 
     parser = argparse.ArgumentParser(description='Add a link to the bookmark list.')
+    parser.add_argument('note', type=str, nargs='*', default='',
+                        help='A note to add.')
     parser.add_argument('--title', metavar='TITLE', type=str, default='',
                         help='the title for the bookmark')
     parser.add_argument('--url', metavar='URL', type=str, default='',
                         help='the url for the bookmark')
     parser.add_argument('--tags', metavar='TAGS', type=str, default='',
-                        help='the (comma-separated) tags for the bookmark')
+                        help='the (comma-separated) tags for the bookmark.  '
+                        'Note also that tags can be added with #hashtags.')
     parser.add_argument('--uzbl', default=is_uzbl, action='store_true',
                         help='assume this is being called from uzbl '
                         '(regarding environment, etc).  '
@@ -67,6 +87,9 @@ def get_args():
                         help='assume this is NOT being called from uzbl')
     parser.add_argument('--stdout', action='store_true', default=False,
                         help='print the result to stdout')
+    parser.add_argument('--file', metavar='FILE', default='bookmarks',
+                        help='The file in {} to write to. (Default: {})'.format(
+                            DEFAULT_OUT_DIR, 'bookmarks'))
 
     args = parser.parse_args()
 
@@ -77,10 +100,12 @@ def get_args():
     args.tags = args.tags.strip().replace(' ', '-')
     args.tags = [t for t in args.tags.split(',')] if args.tags else []
 
+    args.note = ' '.join(args.note)
+
     return args
 
 def get_bmark_from_args(args):
-    bmark = Bookmark(args.url, args.tags, args.title)
+    bmark = Bookmark(args.url, args.tags, args.title, args.note)
     bmark.apply_transforms()
     return bmark
 
@@ -91,7 +116,8 @@ def main():
     if args.stdout:
         print(line, file=sys.stdout)
     else:
-        with open(DEFAULT_OUT, 'a') as fp:
+        path = os.path.join(DEFAULT_OUT_DIR, args.file)
+        with open(path, 'a') as fp:
             print(line, file=fp)
 
 if __name__ == '__main__':
