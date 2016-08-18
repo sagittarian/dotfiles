b53f95a5bc93997e66ce795920eb2bae00dc7462
Adam Mesha <adam@mesha.org>
2016-08-18 13:08:45 +0300

    Many custom.el changes

--- home/.emacs.d/custom.el
+++ home/.emacs.d/custom.el
@@ -9,6 +9,8 @@
  '(ag-arguments
    (quote
     ("--line-number" "--smart-case" "--nogroup" "--column" "--stats" "--ignore" "doc" "--ignore" "jsdoc" "--")))
+ '(ansi-color-names-vector
+   ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#e090d7" "#8cc4ff" "#eeeeec"])
  '(auto-coding-alist
    (quote
     (("\\.\\(arc\\|zip\\|lzh\\|lha\\|zoo\\|[jew]ar\\|xpi\\|rar\\|7z\\|ARC\\|ZIP\\|LZH\\|LHA\\|ZOO\\|[JEW]AR\\|XPI\\|RAR\\|7Z\\)\\'" . no-conversion-multibyte)
@@ -39,18 +41,22 @@
  '(calendar-view-holidays-initially-flag t)
  '(coffee-tab-width 2)
  '(column-number-mode t)
- '(compilation-scroll-output t)
+ '(compilation-auto-jump-to-first-error nil)
+ '(compilation-scroll-output (quote first-error))
+ '(compilation-search-path nil)
  '(css-indent-offset 2)
- '(custom-enabled-themes (quote (manoj-dark)))
+ '(custom-enabled-themes (quote (tango-dark)))
  '(custom-safe-themes
    (quote
-    ("0c29db826418061b40564e3351194a3d4a125d182c6ee5178c237a7364f0ff12" default)))
+    ("3cc2385c39257fed66238921602d8104d8fd6266ad88a006d0a4325336f5ee02" "987b709680284a5858d5fe7e4e428463a20dfabe0a6f2a6146b3b8c7c529f08b" "9b59e147dbbde5e638ea1cde5ec0a358d5f269d27bd2b893a0947c4a867e14c1" "e9776d12e4ccb722a2a732c6e80423331bcb93f02e089ba2a4b02e85de1cf00e" "72a81c54c97b9e5efcc3ea214382615649ebb539cb4f2fe3a46cd12af72c7607" "58c6711a3b568437bab07a30385d34aacf64156cc5137ea20e799984f4227265" "7085ad716baf17384f10a5eb121a840a4c29da57e403e8bceff0ca65a6f83d61" "0c29db826418061b40564e3351194a3d4a125d182c6ee5178c237a7364f0ff12" default)))
  '(delete-selection-mode t)
  '(delete-trailing-lines nil)
  '(desktop-save-mode t)
  '(doc-view-continuous t)
- '(electric-indent-mode nil)
+ '(electric-indent-mode t)
  '(electric-layout-mode nil)
+ '(electric-pair-delete-adjacent-pairs nil)
+ '(electric-pair-preserve-balance nil)
  '(erc-autojoin-channels-alist (quote (("freenode.net" "#emacs"))))
  '(erc-log-channels-directory "~/.log")
  '(erc-log-insert-log-on-open t)
@@ -62,7 +68,7 @@
     (autojoin button completion fill irccontrols list log match menu move-to-prompt netsplit networks noncommands readonly ring stamp track)))
  '(erc-nick "sagittarian")
  '(erc-save-queries-on-quit nil)
- '(fill-column 80)
+ '(fill-column 72)
  '(flex-isearch-auto (quote on-failed))
  '(focus-follows-mouse t)
  '(git-commit-confirm-commit nil)
@@ -108,6 +114,7 @@
  '(ido-use-virtual-buffers t)
  '(indent-tabs-mode nil)
  '(inhibit-startup-screen t)
+ '(jedi:complete-on-dot t)
  '(js2-allow-keywords-as-property-names nil)
  '(js2-basic-offset 4)
  '(js2-bounce-indent-p nil)
@@ -134,7 +141,9 @@
            [org-add-hook change-major-mode-hook org-babel-show-result-all append local]
            5]
      org-babel-result-hide-spec org-babel-hide-all-hashes wc-mode)))
+ '(projectile-enable-caching t)
  '(projectile-global-mode t)
+ '(projectile-sort-order (quote recently-active))
  '(projectile-switch-project-action (quote projectile-dired))
  '(projectile-use-git-grep t)
  '(python-indent-guess-indent-offset t)
@@ -147,11 +156,14 @@
  '(save-place t nil (saveplace))
  '(save-place-file "~/.emacs.d/places")
  '(show-paren-mode t)
+ '(show-trailing-whitespace t)
  '(size-indication-mode t)
+ '(smartparens-global-mode t)
+ '(smartparens-global-strict-mode nil)
+ '(sp-autoescape-string-quote nil)
+ '(sp-hybrid-kill-excessive-whitespace t)
+ '(sp-successive-kill-preserve-whitespace 2)
  '(tab-width 4)
- '(text-mode-hook
-   (quote
-    (er/add-text-mode-expansions text-mode-hook-identify)))
  '(tool-bar-mode nil)
  '(track-eol t)
  '(tramp-default-method "ssh")
@@ -162,6 +174,7 @@
  '(uniquify-strip-common-suffix t)
  '(wdired-allow-to-change-permissions t)
  '(wgrep-enable-key (kbd "C-c C-r"))
+ '(with-editor-emacsclient-executable "/usr/bin/emacsclient")
  '(words-include-escapes t)
  '(yas-global-mode t nil (yasnippet))
  '(yas-triggers-in-field t)
