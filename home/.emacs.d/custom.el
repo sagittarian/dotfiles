8079193df1961bc93fc26bfe61aa7169ebe5db0f
Adam Mesha <adam@mesha.org>
2017-07-29 14:53:45 +0300

    Set ag-group-matches to nil

--- home/.emacs.d/custom.el
+++ home/.emacs.d/custom.el
@@ -9,6 +9,7 @@
  '(ag-arguments
    (quote
     ("--line-number" "--smart-case" "--nogroup" "--column" "--stats" "--ignore" "doc" "--ignore" "jsdoc" "--")))
+ '(ag-group-matches nil)
  '(ansi-color-names-vector
    ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#e090d7" "#8cc4ff" "#eeeeec"])
  '(auto-coding-alist
