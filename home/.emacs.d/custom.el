2a02a8b18e63f5b08ad1ee725404544e424892e5
Adam Mesha <adam@mesha.org>
2018-03-28 20:06:38 +0300

    Add -w to magit diff section arguments

--- home/.emacs.d/custom.el
+++ home/.emacs.d/custom.el
@@ -136,6 +136,7 @@
  '(magit-diff-arguments (quote ("--no-ext-diff" "-w")))
  '(magit-diff-options (quote ("--patience" "-b")))
  '(magit-diff-refine-hunk t)
+ '(magit-diff-section-arguments (quote ("--no-ext-diff" "-w")))
  '(magit-process-popup-time 1)
  '(magit-restore-window-configuration t)
  '(makefile-mode-hook (quote ((lambda nil (set-variable (quote tab-width) 8)))))
