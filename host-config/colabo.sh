e0b69c31ef6e0674f8df26d5c72ef9e30f1d172e
Adam Mesha <adam@mesha.org>
2018-06-06 18:02:58 +0300

    Update colabo settings

--- host-config/colabo.sh
+++ host-config/colabo.sh
@@ -1,6 +1,6 @@
 alias jenkins='ssh -L8080:localhost:8080 adam@c8-ci.colabo.com'
 alias ipy=ipython2
-alias ipy3='[ -n "$VIRTUAL_ENV" ] && deactivate; ipython3'
+alias ipy3='[ -n "$VIRTUAL_ENV" ] && (python --version |& grep "Python 2" > /dev/null) && deactivate; ipython3'
 
 export NVM_DIR="/home/adam/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
@@ -27,6 +27,8 @@ export CDPATH=$CDPATH:$HOME/src/genie/branches:$HOME/src/genie/worktrees
 for pkg in genie pycase; do
     PYTHONPATH=./src/python/$pkg:../src/python/$pkg:$PYTHONPATH
 done
+PYTHONPATH=./oms_svc/src/python/genie_oms:$PYTHONPATH
+PYTHONPATH=./test/src/python/genie_test/genie_test:../test/src/python/genie_test/genie_test:$PYTHONPATH
 export PYTHONPATH
 
 export GENIE_ENV=dev
@@ -39,6 +41,20 @@ function runsvc {
 }
 
 export FIREBASE_PROJECT_ID=dev-adam-12bd0
+export PROD=c8-pr1.colabo.com
+export ST2=c8-st2.colabo.com
+export ST1=c8-st1.colabo.com
+
+alias lorem="python -c 'import random; from statlorem import ipsum; print(ipsum(\"scifi\", 1, random.randrange(5, 19)))' | tee >(cat 1>&2) | xsel"
+
+alias monsvc='watch "ps aux | grep -v virtualenv | grep [s]vc; echo total services: \$(ps aux | grep -v virtualenv | grep [s]vc | wc -l)"'
+
+alias newjira="jira-cli new -v --jira-url https://jira.colabo.com --project GEN --assignee adam --description '' --type Task --priority medium"
+
+export GENIE_GLOBAL_LOG_LEVEL=info
 
 # source $HOME/.virtualenvs/geniedev/bin/activate
 workon geniedev
+
+
+# docker run -v $(readlink -f ./conf):/genie/services/conf -v /var/log/genie:/var/log/genie -ti --entrypoint /bin/bash adam-genie-test
