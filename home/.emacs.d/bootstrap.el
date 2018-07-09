a68c4f3d57a21af56070a46a50e692a1862b77b2
Adam Mesha <adam@mesha.org>
2018-07-09 12:21:06 +0300

    Reformat bootstrap.el

--- home/.emacs.d/bootstrap.el
+++ home/.emacs.d/bootstrap.el
@@ -1,30 +1,31 @@
 (setq package-list
-      '(flx flx-ido
-            ;; flx-isearch ;; seems broken
-            yasnippet projectile magit
-            ido-completing-read+
-            git-timemachine wc-mode js2-mode js2-refactor js-doc
-            ;; uniquify saveplace
-            flycheck flycheck-ghcmod flycheck-haskell flymake-easy
-            flymake-hlint flymake-jshint flymake-json flymake-yaml
-            elpy smex smartparens sublime-themes expand-region
-            yaml-mode ansible-doc ggtags jinja2-mode
-            json-mode json-reformat ag winnow swiper
-            fill-column-indicator
-            ;; undo-tree
-            docker marcopolo dockerfile-mode docker-tramp docker-api
-            ;; isearch+
-            hydra move-text undo-tree git-ps1-mode
-            avy ;; ace-jump-mode ace-jump-buffer ace-window
-            dired-subtree
-            ;; dired-sidebar
-            ;; pylint
-            ;; wiki-summary i3wm
-            git-gutter+ git-gutter-fringe+
-            traad
-            string-inflection
-            smartscan
-            pyvenv which-key))
+      '(;; flx
+        ;; flx-ido
+        ;; flx-isearch ;; seems broken
+        yasnippet projectile magit
+        ;; ido-completing-read+
+        git-timemachine wc-mode js2-mode js2-refactor js-doc
+        ;; uniquify saveplace
+        flycheck flycheck-ghcmod flycheck-haskell flymake-easy
+        flymake-hlint flymake-jshint flymake-json flymake-yaml
+        elpy smex smartparens sublime-themes expand-region
+        yaml-mode ansible-doc ggtags jinja2-mode
+        json-mode json-reformat ag winnow swiper
+        fill-column-indicator
+        ;; undo-tree
+        docker marcopolo dockerfile-mode docker-tramp docker-api
+        ;; isearch+
+        hydra move-text undo-tree git-ps1-mode
+        avy ;; ace-jump-mode ace-jump-buffer ace-window
+        dired-subtree
+        ;; dired-sidebar
+        ;; pylint
+        ;; wiki-summary i3wm
+        git-gutter+ git-gutter-fringe+
+        traad
+        string-inflection
+        smartscan
+        pyvenv which-key))
 
 ;; some others
 ;; json-snatcher magit-annex matlab-mode multiple-cursors noxml-fold
