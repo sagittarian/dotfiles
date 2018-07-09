6220271ba854644120a877c5c9b1d1e866ccd2f5
Adam Mesha <adam@mesha.org>
2018-07-09 17:48:59 +0300

    Comment out a bunch of unused smartparens keybindings

--- home/.emacs.d/load/keybindings.el
+++ home/.emacs.d/load/keybindings.el
@@ -194,18 +194,18 @@
 (define-key smartparens-mode-map (kbd "M-<backspace>") 'sp-backward-unwrap-sexp)
 
 (define-key smartparens-mode-map (kbd "C-<right>") 'sp-forward-slurp-sexp)
-(define-key smartparens-mode-map (kbd "<C-S-right>") 'sp-slurp-hybrid-sexp)
+;; (define-key smartparens-mode-map (kbd "C-S-<right>") 'sp-slurp-hybrid-sexp)
 (define-key smartparens-mode-map (kbd "C-<left>") 'sp-forward-barf-sexp)
-(define-key smartparens-mode-map (kbd "C-M-<left>") 'sp-backward-slurp-sexp)
-(define-key smartparens-mode-map (kbd "C-M-<right>") 'sp-backward-barf-sexp)
+;; (define-key smartparens-mode-map (kbd "C-M-<left>") 'sp-backward-slurp-sexp)
+;; (define-key smartparens-mode-map (kbd "C-M-<right>") 'sp-backward-barf-sexp)
 
-(define-key smartparens-mode-map (kbd "M-D") 'sp-splice-sexp)
-(define-key smartparens-mode-map (kbd "C-M-<delete>") 'sp-splice-sexp-killing-forward)
-(define-key smartparens-mode-map (kbd "C-M-<backspace>") 'sp-splice-sexp-killing-backward)
-(define-key smartparens-mode-map (kbd "C-S-<backspace>") 'sp-splice-sexp-killing-around)
+;; (define-key smartparens-mode-map (kbd "M-D") 'sp-splice-sexp)
+;; (define-key smartparens-mode-map (kbd "C-M-<delete>") 'sp-splice-sexp-killing-forward)
+;; (define-key smartparens-mode-map (kbd "C-M-<backspace>") 'sp-splice-sexp-killing-backward)
+;; (define-key smartparens-mode-map (kbd "C-S-<backspace>") 'sp-splice-sexp-killing-around)
 
 (define-key smartparens-mode-map (kbd "C-]") 'sp-select-next-thing-exchange)
-(define-key smartparens-mode-map (kbd "C-<left_bracket>") 'sp-select-previous-thing)
+;; (define-key smartparens-mode-map (kbd "C-<left_bracket>") 'sp-select-previous-thing)
 (define-key smartparens-mode-map (kbd "C-M-]") 'sp-select-next-thing)
 
 (define-key smartparens-mode-map (kbd "M-F") 'sp-forward-symbol)
