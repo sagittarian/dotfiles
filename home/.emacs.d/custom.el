3f4730d962b4329583009907a39d56253a0e67d1
Adam Mesha <adam@mesha.org>
2018-10-03 12:37:44 +0300

    Fix background color for dired-subtree

--- home/.emacs.d/custom.el
+++ home/.emacs.d/custom.el
@@ -53,7 +53,7 @@
  '(compilation-scroll-output (quote first-error))
  '(compilation-search-path nil)
  '(css-indent-offset 2)
- '(custom-enabled-themes (quote (wheatgrass)))
+ '(custom-enabled-themes (quote (leuven)))
  '(custom-safe-themes
    (quote
     ("3b5ce826b9c9f455b7c4c8bff22c020779383a12f2f57bf2eb25139244bb7290" "e0d42a58c84161a0744ceab595370cbe290949968ab62273aed6212df0ea94b4" "551596f9165514c617c99ad6ce13196d6e7caa7035cea92a0e143dbe7b28be0e" "c48551a5fb7b9fc019bf3f61ebf14cf7c9cdca79bcb2a4219195371c02268f11" "3cc2385c39257fed66238921602d8104d8fd6266ad88a006d0a4325336f5ee02" "987b709680284a5858d5fe7e4e428463a20dfabe0a6f2a6146b3b8c7c529f08b" "9b59e147dbbde5e638ea1cde5ec0a358d5f269d27bd2b893a0947c4a867e14c1" "e9776d12e4ccb722a2a732c6e80423331bcb93f02e089ba2a4b02e85de1cf00e" "72a81c54c97b9e5efcc3ea214382615649ebb539cb4f2fe3a46cd12af72c7607" "58c6711a3b568437bab07a30385d34aacf64156cc5137ea20e799984f4227265" "7085ad716baf17384f10a5eb121a840a4c29da57e403e8bceff0ca65a6f83d61" "0c29db826418061b40564e3351194a3d4a125d182c6ee5178c237a7364f0ff12" default)))
@@ -268,6 +268,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "black" :foreground "wheat" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 108 :width normal :foundry "DAMA" :family "Ubuntu Mono")))))
+ '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 105 :width normal :foundry "DAMA" :family "Ubuntu Mono"))))
+ '(dired-subtree-depth-1-face ((t (:background "white"))))
+ '(dired-subtree-depth-2-face ((t (:background "white smoke"))))
+ '(dired-subtree-depth-3-face ((t (:background "gainsboro"))))
+ '(dired-subtree-depth-4-face ((t (:background "light gray"))))
+ '(dired-subtree-depth-5-face ((t (:background "dark gray"))))
+ '(dired-subtree-depth-6-face ((t (:background "dim gray"))))
+ '(git-gutter+-added ((t (:foreground "dark green" :weight bold)))))
 
 ;;; custom.el ends here
