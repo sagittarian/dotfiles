fc726e3ae7bc66ff1574ce6af13604296aec4fad
Adam Mesha <adam@mesha.org>
2019-04-29 12:44:43 +0300

    Change the git-gutter+ prefix from C-c M-g to C-c u

--- home/.emacs.d/load/keybindings.el
+++ home/.emacs.d/load/keybindings.el
@@ -58,18 +58,18 @@
 (global-set-key (kbd "C-g") 'keyboard-quit)
 
 ;; git-gutter+
-(define-key git-gutter+-mode-map (kbd "C-c M-g n") 'git-gutter+-next-hunk)
-(define-key git-gutter+-mode-map (kbd "C-c M-g p") 'git-gutter+-previous-hunk)
+(define-key git-gutter+-mode-map (kbd "C-c u n") 'git-gutter+-next-hunk)
+(define-key git-gutter+-mode-map (kbd "C-c u p") 'git-gutter+-previous-hunk)
 ;; act on hunks
-(define-key git-gutter+-mode-map (kbd "C-c M-g v =") 'git-gutter+-show-hunk)
-(define-key git-gutter+-mode-map (kbd "C-c M-g r") 'git-gutter+-revert-hunks)
+(define-key git-gutter+-mode-map (kbd "C-c u v =") 'git-gutter+-show-hunk)
+(define-key git-gutter+-mode-map (kbd "C-c u r") 'git-gutter+-revert-hunks)
 ;; stage hunk at point
 ;; if region is active, stage all hunk lines within the region
-(define-key git-gutter+-mode-map (kbd "C-c M-g s") 'git-gutter+-stage-hunks)
-(define-key git-gutter+-mode-map (kbd "C-c M-g c") 'git-gutter+-commit)
-(define-key git-gutter+-mode-map (kbd "C-c M-g C") 'git-gutter+-stage-and-commit)
-(define-key git-gutter+-mode-map (kbd "C-c M-g C-y") 'git-gutter+-stage-and-commit-whole-buffer)
-(define-key git-gutter+-mode-map (kbd "C-c M-g u") 'git-gutter+-unstage-whole-buffer)
+(define-key git-gutter+-mode-map (kbd "C-c u s") 'git-gutter+-stage-hunks)
+(define-key git-gutter+-mode-map (kbd "C-c u c") 'git-gutter+-commit)
+(define-key git-gutter+-mode-map (kbd "C-c u C") 'git-gutter+-stage-and-commit)
+(define-key git-gutter+-mode-map (kbd "C-c u C-y") 'git-gutter+-stage-and-commit-whole-buffer)
+(define-key git-gutter+-mode-map (kbd "C-c u u") 'git-gutter+-unstage-whole-buffer)
 
 ;; custom commands
 (global-set-key (kbd "C-M-&") 'shell-command-as-kill)
