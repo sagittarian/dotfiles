573a7f5a28f6ccdfbd8e92657fe2503d61971d66
Adam Raizen <adam@mesha.org>
2021-10-07 20:46:53 +0300

    Set default face to default

--- home/.emacs.d/custom.el
+++ home/.emacs.d/custom.el
@@ -271,7 +271,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 105 :width normal :foundry "DAMA" :family "Ubuntu Mono"))))
+ '(default ((t (:background nil))))
  '(dired-subtree-depth-1-face ((t (:background "dark orchid"))))
  '(dired-subtree-depth-2-face ((t (:background "purple4"))))
  '(dired-subtree-depth-3-face ((t (:background "navy"))))
