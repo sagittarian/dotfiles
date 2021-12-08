ee60d0e51fc72425c2613c6573fdd36299a44012
Adam Raizen <adam@mesha.org>
2021-12-08 19:46:43 +0200

    Add package to contain use-package declarations

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -21,6 +21,14 @@
   ;; early init and init
   (package-initialize))
 
+;; This is only needed once, near the top of the file
+(eval-when-compile
+  ;; Following line is not needed if use-package.el is in ~/.emacs.d
+  ;; (add-to-list 'load-path "<path where use-package is installed>")
+  (require 'use-package))
+
+
+(load "usepkg")
 
 
 ;; scroll the buffer when moving up/down
---
+++ home/.emacs.d/load/usepkg.el
@@ -0,0 +1,8 @@
+;;; Commentary:
+;; This file is a collection of use-package delarations
+
+;;; Code:
+
+(use-package python)
+
+(provide 'usepkg)
