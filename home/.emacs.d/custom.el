4639b99aa992b7d22198dee7683de6a7e9c9ba29
Adam Raizen <adam@mesha.org>
2022-12-22 08:36:13 +0200

    Turn on font lock mode in various places

--- home/.emacs.d/custom.el
+++ home/.emacs.d/custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(Info-mode-hook '(scroll-lock-mode turn-on-font-lock))
  '(ag-arguments
    '("--line-number" "--smart-case" "--nogroup" "--column" "--stats" "--ignore" "doc" "--ignore" "jsdoc" "--hidden" "--"))
  '(ag-group-matches nil)
@@ -113,6 +114,7 @@
  '(global-undo-tree-mode t)
  '(haskell-font-lock-symbols t)
  '(haskell-mode-hook '(turn-on-haskell-indentation) t)
+ '(help-mode-hook '(scroll-lock-mode))
  '(hippie-expand-try-functions-list
    '(try-complete-file-name-partially try-complete-file-name try-expand-all-abbrevs try-expand-dabbrev try-expand-dabbrev-all-buffers try-expand-dabbrev-from-kill try-complete-lisp-symbol-partially try-complete-lisp-symbol try-expand-line try-expand-list))
  '(holiday-bahai-holidays nil)
@@ -243,6 +245,8 @@
  '(swiper-isearch-highlight-delay '(3 0.5))
  '(swiper-min-highlight 3)
  '(tab-width 4)
+ '(text-mode-hook
+   '(er/add-text-mode-expansions text-mode-hook-identify scroll-lock-mode))
  '(tool-bar-mode nil)
  '(track-eol t)
  '(tramp-default-method "ssh" nil (tramp))
@@ -272,6 +276,7 @@
      (340 . "#a71d5d")
      (360 . "#969896")))
  '(vc-annotate-very-old-color "#969896")
+ '(view-mode-hook '(evil-normalize-keymaps scroll-lock-mode))
  '(w3m-fill-column 72)
  '(w3m-mode-hook '(bookmark-w3m-prepare visual-line-mode))
  '(warning-minimum-level :error)
